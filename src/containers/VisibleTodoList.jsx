bd40521710fc37a7e896a52c871dbf5b07baccad
Piotr Lasota <lasota.piotr1@gmail.com>
2017-10-12 23:46:47 +0200

    Fix bug with filtering todos array

--- src/containers/VisibleTodoList.jsx
+++ src/containers/VisibleTodoList.jsx
@@ -2,43 +2,31 @@ import { connect } from 'react-redux';
 import { toggleTodo } from '../actions';
 import TodoList from '../components/TodoList';
 
-const getVisibileTodos = (todos, filter) => {
+export const getVisibleTodos = (todos, filter) => {
+  const createNewTodos = (acc, id) => ({
+    ...acc,
+    [id]: {
+      ...todos[id],
+    },
+  });
   switch (filter) {
     case 'SHOW_ALL':
       return todos;
     case 'SHOW_COMPLETED':
       return Object.keys(todos)
         .filter(t => todos[t].completed)
-        .map((t) => {
-          console.log(todos[t]);
-          return t;
-        })
-        .reduce((acc, id) => ({
-          ...acc,
-          id: {
-            ...todos[id],
-          },
-        }), {});
+        .reduce(createNewTodos, {});
     case 'SHOW_ACTIVE':
       return Object.keys(todos)
         .filter(t => !todos[t].completed)
-        .map((t) => {
-          console.log(todos[t]);
-          return t;
-        })
-        .reduce((acc, id) => ({
-          ...acc,
-          id: {
-            ...todos[id],
-          },
-        }), {});
+        .reduce(createNewTodos, {});
     default:
       return todos;
   }
 };
 
 const mapStateToProps = state => ({
-  todos: getVisibileTodos(state.todos, state.visibilityFilter),
+  todos: getVisibleTodos(state.todos, state.visibilityFilter),
 });
 
 const mapDispatchToProps = dispatch => ({
