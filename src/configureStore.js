d71dacdb82ef95b064a1cdb6d3ab161a9091d3d1
Piotr Lasota <lasota.piotr1@gmail.com>
2017-10-17 21:54:12 +0200

    Fix working redux-logger and redux extension together

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,27 +1,31 @@
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import throttle from 'lodash/throttle';
 import todoApp from './reducers/todoApp';
 import { loadState, saveState } from './localStorage';
-
 
 const configureStore = () => {
   const persistedState = loadState();
 
   /* eslint-disable no-underscore-dangle */
+  const middleware = [logger];
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     todoApp,
     persistedState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(logger),
+    composeEnhancers(applyMiddleware(...middleware)),
   );
   /* eslint-enable */
 
-  store.subscribe(throttle(() => {
-    saveState({
-      todos: store.getState().todos,
-    });
-  }), 1000);
+  store.subscribe(
+    throttle(() => {
+      saveState({
+        todos: store.getState().todos,
+      });
+    }),
+    1000,
+  );
 
   return store;
 };
