218afe99f3f6126319004fa160e857511c30c32b
Piotr Lasota <lasota.piotr1@gmail.com>
2017-10-19 19:19:15 +0200

    Add error message

--- src/api/index.js
+++ src/api/index.js
@@ -1,52 +1,59 @@
 import uuidv4 from 'uuid/v4';
 
 const fakeDatabase = {
-  todos: [{
-    id: uuidv4(),
-    text: 'hey',
-    completed: true,
-  }, {
-    id: uuidv4(),
-    text: 'ho',
-    completed: true,
-  }, {
-    id: uuidv4(),
-    text: 'let’s go',
-    completed: false,
-  }],
+  todos: [
+    {
+      id: uuidv4(),
+      text: 'hey',
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      text: 'ho',
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      text: 'let’s go',
+      completed: false,
+    },
+  ],
 };
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-export const fetchTodos = filter => delay(500).then(() => {
-  if (Math.random() > 0.95) throw new Error('NOPE!');
+export const fetchTodos = filter =>
+  delay(500).then(() => {
+    if (Math.random() > 0.5) throw new Error('NOPE!');
 
-  switch (filter) {
-    case 'all':
-      return fakeDatabase.todos;
-    case 'active':
-      return fakeDatabase.todos.filter(t => !t.completed);
-    case 'completed':
-      return fakeDatabase.todos.filter(t => t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}`);
-  }
-});
+    switch (filter) {
+      case 'all':
+        return fakeDatabase.todos;
+      case 'active':
+        return fakeDatabase.todos.filter(t => !t.completed);
+      case 'completed':
+        return fakeDatabase.todos.filter(t => t.completed);
+      default:
+        throw new Error(`Unknown filter: ${filter}`);
+    }
+  });
 
-export const addTodo = text => delay(500).then(() => {
-  const todo = {
-    id: uuidv4(),
-    text,
-    completed: false,
-  };
+export const addTodo = text =>
+  delay(500).then(() => {
+    const todo = {
+      id: uuidv4(),
+      text,
+      completed: false,
+    };
 
-  fakeDatabase.todos.push(todo);
+    fakeDatabase.todos.push(todo);
 
-  return todo;
-});
+    return todo;
+  });
 
-export const toggleTodo = id => delay(500).then(() => {
-  const todo = fakeDatabase.todos.find(t => t.id === id);
-  todo.completed = !todo.completed;
-  return todo;
-});
+export const toggleTodo = id =>
+  delay(500).then(() => {
+    const todo = fakeDatabase.todos.find(t => t.id === id);
+    todo.completed = !todo.completed;
+    return todo;
+  });
--- src/containers/VisibleTodoList.jsx
+++ src/containers/VisibleTodoList.jsx
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import * as actions from '../actions';
 import TodoList from '../components/TodoList';
-import { getVisibleTodos, getIsFetching } from '../reducers';
+import { getVisibleTodos, getErrorMessage, getIsFetching } from '../reducers';
+import FetchError from '../components/FetchError';
 
 class VisibleTodoList extends Component {
   componentDidMount() {
@@ -22,10 +23,23 @@ class VisibleTodoList extends Component {
   }
 
   render() {
-    const { toggleTodo, deleteTodo, todos, isFetching } = this.props;
+    const {
+      toggleTodo,
+      deleteTodo,
+      todos,
+      isFetching,
+      errorMessage,
+    } = this.props;
     if (isFetching && !todos.length) {
       return <p>Loading...</p>;
     }
+
+    if (errorMessage && !todos.length) {
+      return (
+        <FetchError message={errorMessage} onRetry={() => this.fetchData()} />
+      );
+    }
+
     return (
       <TodoList
         todos={todos}
@@ -41,6 +55,7 @@ const mapStateToProps = (state, { match }) => {
   return {
     todos: getVisibleTodos(state, filter),
     isFetching: getIsFetching(state, filter),
+    errorMessage: getErrorMessage(state, filter),
     filter,
   };
 };
