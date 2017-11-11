a7db4f9652d14581fe4ea8bae91cd560755400b4
Piotr Lasota <lasota.piotr1@gmail.com>
2017-11-11 23:17:31 +0100

    Update styling of todo - work in progress

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 import Star from './Star';
 import Check from './Check';
+
+const StyledTodo = styled.li`
+  display: flex;
+`;
+
+const TodoBodyFooter = styled.div`
+  min-height: 12px;
+  font-size: 12px;
+  color: gray;
+`;
+
+const TodoContentItem = styled.div`
+  padding-right: 0.5rem;
+  
+  ${({ full }) => full && css`
+     flex-grow: 1;
+	`}
+`;
 
 const Todo = ({
   id,
@@ -23,14 +42,19 @@ const Todo = ({
     onPriorityClick(id);
   };
   return (
-    <li>
-      <Check onClickHandle={onClickTodo} isActive={completed} />
-      <span>
-        {text}{' '}{projectName}
-      </span>
-      <Star onClickHandle={onClickPriorityButton} isActive={priority} />
+    <StyledTodo>
+      <TodoContentItem>
+        <Check onClickHandle={onClickTodo} isActive={completed} />
+      </TodoContentItem>
+      <TodoContentItem full>
+        <div>{text}</div>
+        <TodoBodyFooter>{projectName}</TodoBodyFooter>
+      </TodoContentItem>
+      <TodoContentItem>
+        <Star onClickHandle={onClickPriorityButton} isActive={priority} />
+      </TodoContentItem>
       <button onClick={onClickDeleteButton}>X</button>
-    </li>
+    </StyledTodo>
   );
 };
 
