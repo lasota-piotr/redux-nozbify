ce60dabb3ba3296af7f808fc19a37eeecb35d9bb
Piotr Lasota <lasota.piotr1@gmail.com>
2017-11-12 21:54:57 +0100

    Simplify layout styled component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledComponent = styled.div`
+const Layout = styled.div`
   max-width: 40em;
   margin-left: auto;
   margin-right: auto;
 `;
-
-const Layout = props => (
-  <StyledComponent {...props} />
-);
 
 export default Layout;
